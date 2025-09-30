2655f2b6e14da8c56c3287bdbc3e115d044cc6bc
ahmdrzlap327 <ahmdrzlap327@gmail.com>
2025-09-30 14:40:46 +0700

    s

--- test.java
+++ test.java
@@ -3,5 +3,6 @@ public class test {
 public static void main(String[] args) {
     System.out.println("Hello, World!");
     System.out.println(" anjeng");
+    System.out.println("ajbsjavcjadsc");
 }
 }
